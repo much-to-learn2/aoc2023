95374700babc87257d95f944cee506fd4408bcf2
matt <mattdgraham95@gmail.com>
2023-12-04 14:35:43 -0500

    cleanup

--- src/day04.rs
+++ src/day04.rs
@@ -1,22 +1,13 @@
 use std::fs;
 use regex::Regex;
 
-// represent a number, storing line and span
-#[derive(Debug)]
-struct Number {
-    value: i32,
-    line: i32,
-    start: i32,
-    end: i32,
-}
-
 struct Card {
-    tickets: Vec<Number>,
-    winners: Vec<Number>,
+    tickets: Vec<i32>,
+    winners: Vec<i32>,
 }
 
 impl Card {
-    fn new(tickets: Vec<Number>, winners: Vec<Number>) -> Self {
+    fn new(tickets: Vec<i32>, winners: Vec<i32>) -> Self {
        Self {
            tickets,
            winners,
@@ -25,7 +16,7 @@ impl Card {
 
     fn num_winners(&self) -> i32 {
         self.tickets.iter().fold(0, |acc, ticket| {
-            if self.winners.iter().any(|winner| winner.value == ticket.value) {
+            if self.winners.iter().any(|winner| winner == ticket) {
                 acc + 1
             } else {
                 acc
@@ -78,35 +69,22 @@ impl Game {
 
 }
 
-
-fn parse_line(s: &str, line: i32) -> (Vec<Number>, Vec<Number>) {
-    let mut tickets: Vec<Number> = vec![];
-    let mut winners: Vec<Number> = vec![];
+fn parse_line(s: &str) -> Card {
+    let mut tickets: Vec<i32> = vec![];
+    let mut winners: Vec<i32> = vec![];
     let s = &s[2+s.find(": ").unwrap()..];
     let mut rounds = s.split(" | ");
     let number_re: &str = r"\d+";
     let re = Regex::new(number_re).unwrap();
     let it = re.find_iter(rounds.next().unwrap());
     for m in it {
-        let number = Number {
-            value: m.as_str().parse::<i32>().unwrap(),
-            line: line,
-            start: m.start() as i32,
-            end: (m.end() - 1) as i32,
-        };
-        winners.push(number);
+        winners.push(m.as_str().parse::<i32>().unwrap());
     }
     let it = re.find_iter(rounds.next().unwrap());
     for m in it {
-        let number = Number {
-            value: m.as_str().parse::<i32>().unwrap(),
-            line: line,
-            start: m.start() as i32,
-            end: (m.end() - 1) as i32,
-        };
-        tickets.push(number);
+        tickets.push(m.as_str().parse::<i32>().unwrap());
     }
-    (winners, tickets)
+    Card::new(winners, tickets)
 }
 
 pub fn main() {
@@ -114,9 +92,8 @@ pub fn main() {
     let contents = fs::read_to_string("inputs/day04.txt").expect("unable to read file contents");
     let mut part1 = 0;
     let mut cards: Vec<Card> = vec![];
-    for (i, line) in contents.lines().enumerate() {
-        let (mut w, mut t) = parse_line(line, i as i32);
-        let card = Card::new(w, t);
+    for line in contents.lines() {
+        let card = parse_line(line);
         part1 += card.score();
         cards.push(card);
     }
